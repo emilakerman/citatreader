f0038c1d86d2de7adaa59bd93aca519349ae2260
emilakerman <emilaakerman@gmail.com>
2024-10-30 17:10:09 +0100

    fixed so it saves and loads the entire list of quoets from sharedprefs

--- app/src/main/java/com/emilakerman/citatreader/MainActivity.kt
+++ app/src/main/java/com/emilakerman/citatreader/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.emilakerman.citatreader
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.emilakerman.citatreader.databinding.ActivityMainBinding
-import java.io.File
 import java.io.InputStream
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val quoteText by lazy { binding.textQuote; }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val quoteText = binding.textQuote;
         val nextButton = binding.nextButton;
         val prevButton = binding.prevButton;
         val exitButton = binding.exitButton;
         val deleteButton = binding.deleteButton;
         val lineList = mutableListOf<String>()
-        var currentQuote = retrieveFromSharedPreferences();
+        var currentQuote = retrieveIndexFromSharedPreferences();
 
+        // Reads the file and initializes the first text.
         segmentInputStream(lineList);
-        quoteText.text = lineList[currentQuote];
+        changeQuoteText(currentQuote, lineList);
 
         if (currentQuote == 0) {
             prevButton.isEnabled = false;
@@ -30,19 +33,16 @@ class MainActivity : AppCompatActivity() {
         // TODO: Delete from local storage instead maybe
         deleteButton.setOnClickListener {
             lineList.removeAt(currentQuote);
-            quoteText.text = lineList[currentQuote];
+            changeQuoteText(currentQuote, lineList);
         }
-        exitButton.setOnClickListener {
-            this.finishAffinity();
-            saveCurrentIndexToSharedPreferences(currentQuote);
-        }
+        exitButton.setOnClickListener { exitAndSave(currentQuote, lineList) }
         nextButton.setOnClickListener {
             if (currentQuote == lineList.lastIndex - 1) {
                 nextButton.isEnabled = false;
             }
             prevButton.isEnabled = true;
             currentQuote++;
-            quoteText.text = lineList[currentQuote];
+            changeQuoteText(currentQuote, lineList);
         }
         prevButton.setOnClickListener {
             if (currentQuote == lineList.lastIndex - 1) {
@@ -55,9 +55,25 @@ class MainActivity : AppCompatActivity() {
                 currentQuote = 0;
             } else {
                 currentQuote--;
-                quoteText.text = lineList[currentQuote];
+                changeQuoteText(currentQuote, lineList);
             }
         }
+    }
+    private fun saveListToSharedPreferences(lineList: MutableList<String>) {
+        val sharedPreference = getSharedPreferences("SAVED_QUOTES", Context.MODE_PRIVATE)
+        val editor = sharedPreference.edit()
+        val json = Gson().toJson(lineList)
+        editor.putString("savedQuotes", json)
+        editor.apply()
+    }
+
+    private fun changeQuoteText(currentQuote: Int, lineList: MutableList<String>) {
+        quoteText.text = lineList[currentQuote];
+    }
+    private fun exitAndSave(currentQuote: Int, lineList: MutableList<String>) {
+        saveCurrentIndexToSharedPreferences(currentQuote);
+        saveListToSharedPreferences(lineList)
+        this.finishAffinity();
     }
     private fun saveCurrentIndexToSharedPreferences(currentQuote: Int) {
         val sharedPreference = getSharedPreferences("SAVED_INDEX",Context.MODE_PRIVATE)
@@ -65,15 +81,30 @@ class MainActivity : AppCompatActivity() {
         editor.putInt("currentQuote",currentQuote)
         editor.apply();
     }
-    private fun retrieveFromSharedPreferences(): Int {
+    private fun retrieveIndexFromSharedPreferences(): Int {
         val sharedPreference = getSharedPreferences("SAVED_INDEX", Context.MODE_PRIVATE)
         return sharedPreference.getInt("currentQuote", 0)
     }
+    private fun retrieveQuotesFromSharedPreferences(): MutableList<String> {
+        val sharedPreference = getSharedPreferences("SAVED_QUOTES", Context.MODE_PRIVATE)
+        val json = sharedPreference.getString("savedQuotes", null)
+        return if (json != null) {
+            val type = object : TypeToken<MutableList<String>>() {}.type
+            Gson().fromJson(json, type)
+        } else {
+            mutableListOf()
+        }
+    }
     private fun segmentInputStream(lineList: MutableList<String>) {
-        val inputStream: InputStream = applicationContext.assets.open("cites.txt")
-        inputStream.bufferedReader().use { reader ->
-            reader.forEachLine { line ->
-                lineList.add(line)
+        val quotes = retrieveQuotesFromSharedPreferences()
+        if (quotes.isNotEmpty()) {
+            lineList.addAll(quotes);
+        } else {
+            val inputStream: InputStream = assets.open("cites.txt")
+            inputStream.bufferedReader().use { reader ->
+                reader.forEachLine { line ->
+                    lineList.add(line)
+                }
             }
         }
     }
